43ea5989931826f7bc88d468e618453c8a1f75de
YoussefJemmane <JEMMANE.YOUSSEF@ofppt-edu.ma>
2022-11-15 00:48:02 +0100

    update

--- dice/dice/src/store/index.js
+++ dice/dice/src/store/index.js
@@ -5,7 +5,7 @@ import {createStore} from 'redux';
 const diceReducer = (state = {dice: 1, essay: 12, cache: 4}, action) => {
   switch (action.type) {
     case 'roll':
-      return {dice: Math.floor(Math.random() * 6 + 1), essay: Math.max(state.essay - 1, 0)};
+      return {dice: Math.floor(Math.random() * 6 + 1), essay: Math.max(state.essay - 1, 0), cache: 4};
     case 'reset':
       return {dice: 1, essay: 12};
     default:
